26acfaf74d3f5caacc7df205a2a028aa73354806
Enrico Bottani <Enrico.Bottani@adeya.ch>
2012-01-30 12:09:41 +0100

    Initial Commit

---
+++ GITTest/EBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  EBAppDelegate.h
+//  GITTest
+//
+//  Created by Enrico Bottani on 30/01/12.
+//  Copyright (c) 2012 Adeya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GITTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GITTest
+//
+//  Created by Enrico Bottani on 30/01/12.
+//  Copyright (c) 2012 Adeya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "EBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([EBAppDelegate class]));
+    }
+}
